330bff8bf237c578e350b2fdedc939fd51de8292
Diego Gaviola <dgaviola@slingr.io>
2024-12-19 12:20:59 -0300

    Adding support for uploading files

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,6 @@
+let httpReference = dependencies.http;
+
+
 /**
  * Gets a configuration from the properties.
  *
@@ -52,4 +55,67 @@ exports.generateContentFromText = function(prompt, options) {
         body: requestBody
     });
     return res.candidates[0].content.parts[0].text;
+};
+
+
+/**
+ * This function uploads a file, given its ID.
+ *
+ * @param {string} fileId - The unique identifier of the file to be uploaded.
+ *
+ * @returns {string} - The URI of the uploaded file
+ */
+exports.uploadFile = function (fileId) {
+    let fileContentType, fileName;
+    let file = sys.files.open(fileId);
+    try {
+        fileName = file.descriptor().name();
+        fileContentType = file.descriptor().type();
+    } finally {
+        file.close();
+    }
+    let res = pkg.googlegemini.api.post({
+        path: `/upload/v1beta/files`,
+        headers: {
+            'X-Goog-Upload-Protocol': 'resumable',
+            'X-Goog-Upload-Command': 'start',
+            'X-Goog-Upload-Header-Content-Type': fileContentType
+        },
+        body: {
+            file: {
+                displayName: fileName
+            }
+        },
+        settings: {
+            fullResponse: true
+        }
+    });
+
+    let uploadUrl = res.headers['X-Goog-Upload-URL'];
+    sys.logs.debug('[google-gemini] Uploading file to URL: ' + uploadUrl);
+
+    // We need to make a call outside the domain, so we need to use the HTTP service directly
+    res = httpReference.post({
+        url: uploadUrl,
+        headers: {
+            'X-Goog-Upload-Offset': '0',
+            'X-Goog-Upload-Command': 'upload, finalize'
+        },
+        params: {
+            key: config.get("apiKey")
+        },
+        settings: {
+            multipart: true,
+            fullResponse: true,
+            parts: [
+                {
+                    name: 'file',
+                    type: 'file',
+                    fileId: fileId
+                }
+            ]
+        }
+    });
+
+    return res.body.file.uri;
 };
